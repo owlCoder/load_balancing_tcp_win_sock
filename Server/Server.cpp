e4a6a3d2d0ffa394d91cd80e6e90b8bba724f49c
Danijel Jovanović <danijel.xda@gmail.com>
2023-12-10 20:06:08 +0100

    Add multi client support

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -6,12 +6,54 @@
 #include <ws2tcpip.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <process.h> // For _beginthreadex
 #include "../Common/MeasurementData.hpp"
-    
+
 #pragma comment(lib, "ws2_32.lib")
 
 #define PORT 5059
 #define MAX_CLIENTS 10
+
+void HandleClient(void* clientSocketPtr) {
+    SOCKET clientSocket = *((SOCKET*)clientSocketPtr);
+    free(clientSocketPtr);
+
+    char buffer[1024];
+    int bytesReceived;
+
+    do {
+        // Allocate memory for new_data
+        struct MeasurementData* new_data = (struct MeasurementData*)malloc(sizeof(struct MeasurementData));
+
+        if (new_data == NULL) {
+            send(clientSocket, "Measurement can't be processed right now\n", 42, 0);
+        }
+        else {
+            bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
+            if (bytesReceived > 0 && bytesReceived < 1023) {
+                buffer[bytesReceived] = '\0';
+
+                if (sscanf(buffer, "%s %u", new_data->date, &(new_data->measurementValue)) == 2) {
+                    // TO DO: ADD TO QUEUE
+                    ////////////////////////////////////
+                    printf("[Server]: New measurement added into queue\n");
+
+                    send(clientSocket, "Measurement added to queue\n", 27, 0);
+                }
+                else {
+                    send(clientSocket, "Error parsing the received measurement\n", 40, 0);
+                }
+            }
+            else {
+                send(clientSocket, "Error receiving the packet from network\n", 41, 0);
+            }
+            free(new_data);
+        }
+    } while (bytesReceived > 0);
+
+    printf("Client disconnected\n");
+    closesocket(clientSocket);
+}
 
 int main() {
     WSADATA wsaData;
@@ -48,14 +90,11 @@ int main() {
 
     printf("[Server]: Listening on port %d\n", PORT);
 
-    /////////////////////////////// TO Do
-    // START BACKGROUND PROCESS TO WORKWITH QUEUE OF DATA TO BALACE ON WORKERS
-
-    SOCKET clientSocket;
-    struct sockaddr_in clientAddr;
-    int clientAddrSize = sizeof(clientAddr);
-
     while (1) {
+        SOCKET clientSocket;
+        struct sockaddr_in clientAddr;
+        int clientAddrSize = sizeof(clientAddr);
+
         if ((clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &clientAddrSize)) == INVALID_SOCKET) {
             fprintf(stderr, "Accept failed with error: %d\n", WSAGetLastError());
             closesocket(serverSocket);
@@ -65,43 +104,11 @@ int main() {
 
         printf("[Server]: Client connected\n");
 
-        // Receive and send data
-        char buffer[1024];
-        int bytesReceived = -1;
+        // Create a thread to handle the client
+        SOCKET* clientSocketPtr = (SOCKET*)malloc(sizeof(SOCKET));
+        *clientSocketPtr = clientSocket;
 
-        do {
-            // Allocate memory for new_data
-            struct MeasurementData* new_data = (struct MeasurementData*)malloc(sizeof(struct MeasurementData));
-
-            if (new_data == NULL) {
-                send(clientSocket, "Measurement can't be processed right now\n", 42, 0);
-            }
-            else {
-                bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
-                if (bytesReceived > 0 && bytesReceived < 1023) {
-                    buffer[bytesReceived] = '\0';
-
-                    if (sscanf(buffer, "%s %u", new_data->date, &(new_data->measurementValue)) == 2) {
-                        
-                        // TO DO ADD TO QUEUE 
-                        // TO DO TO DO
-                        ////////////////////////////////////
-                        printf("[Server]: New measurement added into queue\n");
-
-                        send(clientSocket, "Measurement evidented in queue\n", 32, 0);
-                    }
-                    else {
-                        send(clientSocket, "Error parsing the received measurement\n", 40, 0);
-                    }
-                }
-                else {
-                    send(clientSocket, "Error receiveng the packet from network\n", 41, 0);
-                }
-            }
-        } while (bytesReceived > 0);
-
-        printf("Client disconnected\n");
-        closesocket(clientSocket);
+        _beginthreadex(NULL, 0, (_beginthreadex_proc_type)HandleClient, clientSocketPtr, 0, NULL);
     }
 
     closesocket(serverSocket);
